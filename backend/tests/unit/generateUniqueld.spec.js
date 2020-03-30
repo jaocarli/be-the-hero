2eac31eb9d55dcc874d2573152dab18fb8032f6c
João Carli <46573038+fl4m3x@users.noreply.github.com>
2020-03-30 18:55:19 -0300

    Delete generateUniqueld.spec.js

--- backend/tests/unit/generateUniqueld.spec.js
+++
@@ -1,8 +0,0 @@
-const generateUniqueId = require('../../src/utils/generateUniqueId');
-
-describe('Generate Unique ID', () => {
-  it('should generate an unique ID', () => {
-    const id = generateUniqueId();
-    expect(id).toHaveLength(8);
-  })
-});
